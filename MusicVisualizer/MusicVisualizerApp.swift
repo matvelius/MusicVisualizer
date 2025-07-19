c053515a65a21b1b2b6a1f7b1c05caf508710d1d
Matvey Kostukovsky <email@matveycodes.com>
2025-07-19 12:05:59 -0700

    Initial Commit

---
+++ MusicVisualizer/MusicVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicVisualizerApp.swift
+//  MusicVisualizer
+//
+//  Created by Matvey Kostukovsky on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicVisualizerTests/MusicVisualizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicVisualizerTests.swift
+//  MusicVisualizerTests
+//
+//  Created by Matvey Kostukovsky on 7/19/25.
+//
+
+import Testing
+@testable import MusicVisualizer
+
+struct MusicVisualizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
